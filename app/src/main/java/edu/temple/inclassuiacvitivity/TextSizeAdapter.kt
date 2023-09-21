24dccfd5d7509ba25bf02d385b99835533b8db06
heronziegel <tuo12440@temple.edu>
2023-09-21 15:11:02 -0400

    Distinguished custom adapter's getView from getDropDownView in order to make the selected text a single size rather than variable size

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -16,6 +16,12 @@ class TextSizeAdapter(private val context: Context, private val textSizes: Array
     override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return (getDropDownView(position,convertView,parent) as TextView).apply {
+            textSize = 22f
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val textView: TextView
 
         if(convertView != null) {
